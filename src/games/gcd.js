e3ab99ef248592a96f915029ef456cb403defa64
Andrey Mayuk <mayukda@gmail.com>
2022-12-28 14:10:01 +1000

    Update src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,32 +1,21 @@
-import _ from 'lodash';
 import getRandomInt from '../getRandomInt.js';
 import runGameCore from '../index.js';
 
 const gameRule = 'Find the greatest common divisor of given numbers.';
 
-const formDivisors = (number, array) => {
-  for (let i = 2; i <= number; i += 1) {
-    if (number % i === 0) {
-      array.push(i);
-    }
+const getGcd = (num1, num2) => {
+  if (num1 === 0) {
+    return num2;
   }
+  return getGcd(num2 % num1, num1);
 };
 
 const generateGameData = () => {
   const number1 = getRandomInt(2, 50);
   const number2 = getRandomInt(2, 50);
 
-  const divisorsNumber1 = [];
-  const divisorsNumber2 = [];
-
-  formDivisors(number1, divisorsNumber1);
-  formDivisors(number2, divisorsNumber2);
-
-  const divisors = _.intersection(divisorsNumber1, divisorsNumber2);
-  const maxDivisor = _.max(divisors);
-
   const question = `${number1} ${number2}`;
-  const correctAnswer = divisors.length === 0 ? 1 : maxDivisor;
+  const correctAnswer = getGcd(number1, number2);;
 
   return [question, String(correctAnswer)];
 };
